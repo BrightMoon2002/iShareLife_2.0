c60b810d71391dfa113fc42ffce9cafdcebca0ac
hieuhvh1994 <hieudaohn94@gmail.com>
2022-01-15 00:23:28 +0700

    Added posting with images

--- src/app/posting/upload-images/upload-images.component.ts
+++ src/app/posting/upload-images/upload-images.component.ts
@@ -7,20 +7,20 @@ import {AngularFireStorage, AngularFireStorageReference} from '@angular/fire/sto
   styleUrls: ['./upload-images.component.scss']
 })
 export class UploadImagesComponent implements OnInit {
-  selectedFile: File[];
+  selectedFile: File;
   ref: AngularFireStorageReference;
   downloadUrl: string;
   checkUpLoadFile = false;
   myMap = new Map();
   @Output()
-  giveUrlToCreate = new EventEmitter<string>();
+  giveUrlToCreate = new EventEmitter<string[]>();
   images: Array<string> = [];
   constructor(private afStorage: AngularFireStorage) { }
 
   ngOnInit(): void {
   }
   onFileChange($event) {
-    this.selectedFile = $event.target.files;
+    this.selectedFile = $event.target.files[0];
     this.onUpload();
   }
 
@@ -29,20 +29,17 @@ export class UploadImagesComponent implements OnInit {
     const  id = Math.random().toString(36).substring(2); // tạo ra 1 cái ten random trên firebase
     this.ref = this.afStorage.ref(id);
     // this.ref.put(this.selectedFile)
-    for (let i = 0; i < this.selectedFile.length; i++) {
-      this.ref.put(this.selectedFile[i]).then(snapshot => { // trả về 1 chuỗi siêu văn bản
+    this.ref.put(this.selectedFile).then(snapshot => { // trả về 1 chuỗi siêu văn bản
         return snapshot.ref.getDownloadURL();
       }).then(downloadURL => {
         this.checkUpLoadFile = false;
         this.downloadUrl = downloadURL;
-        this.giveUrlToCreate.emit(this.downloadUrl);
         this.images.push(downloadURL);
-        console.log(this.images);
+        this.giveUrlToCreate.emit(this.images);
       })
         .catch(error => {
           console.log(`Failed to upload avatar! ${error}`);
         });
     }
 
-  }
 }
