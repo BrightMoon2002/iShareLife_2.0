bc14648c6bfceba035c3503f04fdf818263ada31
batuanr <tuanpesi@gmail.com>
2022-01-14 21:20:09 +0700

    add messenger

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -46,6 +46,7 @@ import { FriendComponent } from './profile/friend/friend.component';
 import { TimeLineComponent } from './profile/time-line/time-line.component';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import { PostingCreateComponent } from './posting/posting/posting-create/posting-create.component';
+import { MessengerComponent } from './messenger/messenger.component';
 
 
 
@@ -70,7 +71,8 @@ import { PostingCreateComponent } from './posting/posting/posting-create/posting
     InfoComponent,
     FriendComponent,
     TimeLineComponent,
-    PostingCreateComponent
+    PostingCreateComponent,
+    MessengerComponent
   ],
   imports: [
     HttpClientModule,
