cbf4e43eb75568ff353d1384dd0125f8153e575a
nhimdom200292 <kiemda02@gmail.com>
2022-01-19 02:31:31 +0700

    add idGuest in message model

--- src/app/model/chat/Message.ts
+++ src/app/model/chat/Message.ts
@@ -1,15 +1,15 @@
 export class Message {
   id?: number;
   content?: string;
+  idGuest?: number;
   dateSend?: string;
   nameReceiver?: string;
   nameSender?: string;
-
-
-  constructor(id?: number, content?: string, dateSender?: string, nameReceiver?: string, nameSender?: string) {
+  constructor(id?: number, content?: string, idGuest?: number, dateSend?: string, nameReceiver?: string, nameSender?: string) {
     this.id = id;
     this.content = content;
-    this.dateSend = dateSender;
+    this.idGuest = idGuest;
+    this.dateSend = dateSend;
     this.nameReceiver = nameReceiver;
     this.nameSender = nameSender;
   }
